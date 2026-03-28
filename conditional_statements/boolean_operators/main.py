c1dcbd5af86c9795baf770f77d5a39d90ee889b5
LanceMcGuire <>
2026-03-28 04:41:50 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,9 @@
+#Define a variable named total_cost and assign it the value 25.00 to represent the total cost of a grocery bill.
+total_cost = 25.00
+discount_threshold = 20.00
+#Create a boolean variable named discountEligible by comparing the total_cost variable to the discount threshold of 20.00 using the greater than or equal to (>=) operator.
+discountEligible = total_cost >= discount_threshold
+#Print the value of the discountEligible variable to indicate whether the purchase is eligible for a discount.
+print(discountEligible) 
+#Print the message: Is the purchase eligible for a discount? <discountEligible>.
+print("Is the purchase eligible for a discount?", discountEligible)
